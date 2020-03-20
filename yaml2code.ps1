f477737e8ca8d68d5b3bcb420922a193a34b7565
Colin Cook <colinccook@gmail.com>
2020-03-20 09:52:37 +0000

    does more stuff now

--- yaml2code.ps1
+++ yaml2code.ps1
@@ -1,23 +1,30 @@
 # Import-Module powershell-yaml
 
-Remove-Item –path $solution –recurse
-
 [string[]]$fileContent = Get-Content "code.yaml"
 $content = ''
 foreach ($line in $fileContent) { $content = $content + "`n" + $line }
 $yaml = ConvertFrom-YAML $content
 
-$solution = $yaml["solution"]
+$guid = New-Guid
+$solution = $guid.Guid.ToString()
+# $solution = $yaml["solution"] briefly commented this out to avoid the pain of rebuilding
 $projects = $yaml["projects"]
 
 mkdir $solution
 
 dotnet new sln -o "$solution"
 
-$projects | ForEach-Object {
-    $project = $_.name
-    dotnet new classlib --no-restore -o "$solution/$project"
-    dotnet sln "$solution/$solution.sln" add "$solution/$project/$project.csproj"
-}
+foreach ($project in $projects) {
+    $projectName = $project.name
+    $template = if ($null -eq $project.template) { "classlib" } else { $project.template } 
+    dotnet new $template --no-restore -o "$solution/$projectName"
+    dotnet sln "$solution/$solution.sln" add "$solution/$projectName/$projectName.csproj"
 
-Remove-Item –path $solution –recurse
+    foreach ($reference in $project.references) {
+        dotnet add "$solution/$projectName/$projectName.csproj" reference "$solution/$reference/$reference.csproj"
+    }
+
+    foreach ($package in $project.packages) {
+        dotnet add "$solution/$projectName/$projectName.csproj" package $package
+    }
+}
