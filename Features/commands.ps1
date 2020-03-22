85e25eb58984b482967b329e5714c48244e3cb92
Colin Cook <colinccook@gmail.com>
2020-03-22 19:37:57 +0000

    command and query handlers

--- Features/commands.ps1
+++ Features/commands.ps1
@@ -3,7 +3,19 @@ dotnet add "$solution/$projectName/$projectName.csproj" package CommandQuery
 $commands = $feature.Values.ToArray()
 
 $commandTemplate = Invoke-WebRequest -URI https://raw.githubusercontent.com/hlaueriksson/CommandQuery/master/samples/CommandQuery.Sample.Contracts/Commands/BazCommand.cs
+# Fix namespace
 $commandTemplate = $commandTemplate.Content.Replace("CommandQuery.Sample.Contracts", "$projectName")
+
+$commandHandlerTemplate = Invoke-WebRequest -URI https://raw.githubusercontent.com/hlaueriksson/CommandQuery/master/samples/CommandQuery.Sample.Handlers/Commands/BazCommandHandler.cs
+# Fix namespace
+$commandHandlerTemplate = $commandHandlerTemplate.Content.Replace("CommandQuery.Sample.Handlers", "$projectName")
+$commandHandlerTemplate = $commandHandlerTemplate.Replace("using CommandQuery.Sample.Contracts.Commands;", "")
+# Remove ICultureService example
+$commandHandlerTemplate = $commandHandlerTemplate.Replace("        private readonly ICultureService _cultureService;", "")
+$commandHandlerTemplate = $commandHandlerTemplate.Replace("ICultureService cultureService", "")
+$commandHandlerTemplate = $commandHandlerTemplate.Replace("            _cultureService = cultureService;", "")
+$commandHandlerTemplate = $commandHandlerTemplate.Replace("_cultureService.SetCurrentCulture(command.Value);", "")
+
 
 mkdir "$solution/$projectName/Commands/"
 
@@ -11,4 +23,8 @@ foreach ($command in $commands) {
     $commandCode = $commandTemplate.Replace("Baz", $command);
     New-Item -Path "$solution/$projectName/Commands/$($command)Command.cs"
     Add-Content -Path "$solution/$projectName/Commands/$($command)Command.cs" -Value "using CommandQuery;`r`n`r`n$commandCode"
+
+    $commandHandlerCode = $commandHandlerTemplate.Replace("Baz", $command);
+    New-Item -Path "$solution/$projectName/Commands/$($command)CommandHandler.cs"
+    Add-Content -Path "$solution/$projectName/Commands/$($command)CommandHandler.cs" -Value "using CommandQuery;`r`n`r`n$commandHandlerCode"
 }
--- Features/queries.ps1
+++ Features/queries.ps1
@@ -5,10 +5,25 @@ $queries = $feature.Values.ToArray()
 $queryTemplate = Invoke-WebRequest -URI https://raw.githubusercontent.com/hlaueriksson/CommandQuery/master/samples/CommandQuery.Sample.Contracts/Queries/BarQuery.cs
 $queryTemplate = $queryTemplate.Content.Replace("CommandQuery.Sample.Contracts", "$projectName")
 
+$queryHandlerTemplate = Invoke-WebRequest -URI https://raw.githubusercontent.com/hlaueriksson/CommandQuery/master/samples/CommandQuery.Sample.Handlers/Queries/BarQueryHandler.cs
+# Fix namespace
+$queryHandlerTemplate = $queryHandlerTemplate.Content.Replace("CommandQuery.Sample.Handlers", "$projectName")
+$queryHandlerTemplate = $queryHandlerTemplate.Replace("using CommandQuery.Sample.Contracts.Queries;", "")
+
+# Remove IDateTimeProxy example
+$queryHandlerTemplate = $queryHandlerTemplate.Replace("private readonly IDateTimeProxy _dateTime;", "")
+$queryHandlerTemplate = $queryHandlerTemplate.Replace("IDateTimeProxy dateTime", "")
+$queryHandlerTemplate = $queryHandlerTemplate.Replace("_dateTime = dateTime;", "")
+$queryHandlerTemplate = $queryHandlerTemplate.Replace("_dateTime.Now.ToString(""F"")", """ReturnMessage""")
+
 mkdir "$solution/$projectName/Queries/"
 
 foreach ($query in $queries) {
     $queryCode = $queryTemplate.Replace("Bar", $query);
     New-Item -Path "$solution/$projectName/Queries/$($query)Query.cs"
     Add-Content -Path "$solution/$projectName/Queries/$($query)Query.cs" -Value "using CommandQuery;`r`n`r`n$queryCode"
+
+    $queryHandlerCode = $queryHandlerTemplate.Replace("Bar", $query);
+    New-Item -Path "$solution/$projectName/Queries/$($query)QueryHandler.cs"
+    Add-Content -Path "$solution/$projectName/Queries/$($query)QueryHandler.cs" -Value "using CommandQuery;`r`n`r`n$queryHandlerCode"
 }
